4663715acffab72ac1a03bb33a6f7e6391d9d590
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-09-02 01:09:05 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int m, n, k;
+int a[101][101];
+int square[101][101];
+int lx, ly, rx, ry;
+int num;
+int area;
+vector<int> v;
+
+void dfs(int y, int x)
+{
+    square[y][x] = 1;
+    area++;
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || nx < 0 || ny >= m || nx >= n)
+            continue;
+        if (square[ny][nx] == 0)
+        {
+            dfs(ny, nx);
+        }
+    }
+    return;
+}
+
+int main()
+{
+    cin >> m >> n >> k;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            a[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        cin >> lx >> ly >> rx >> ry;
+        ly = m - ly;
+        ry = m - ry;
+        for (int sy = ry; sy < ly; sy++)
+        {
+            for (int ex = lx; ex < rx; ex++)
+            {
+                square[sy][ex] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (square[i][j] == 0)
+            {
+                dfs(i, j);
+                num++;
+                v.push_back(area);
+                area = 0;
+            }
+            
+        }
+    }
+    cout << num << '\n';
+    sort(v.begin(), v.end());
+    for(int i: v) cout << i << ' ';
+    return 0;
+}
