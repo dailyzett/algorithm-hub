5269fd1ccf05b2143ec949f29986d8f425ab9e4f
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-09-16 18:22:47 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,55 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- *
- */
-
-int n;
-int one;
-string s, prevValue;
+int n, o;
+string s, pre;
 int aScore, bScore;
 int aSum, bSum;
 
-int changeToInt(string a)
-{
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
+int changeToInt(string s) {
+    return atoi(s.substr(0, 2).c_str()) * 60 + atoi(s.substr(3,2).c_str());
 }
 
-void go(int &sum, string s)
-{
-    sum += (changeToInt(s) - changeToInt(prevValue));
+void go(int &sum, string s) {
+    sum += (changeToInt(s) - changeToInt(pre));
 }
 
-string printTime(int z)
-{
-    int hour = z / 60;
-    int minutes = z % 60;
-    string s1 = "00" + to_string(hour);
-    string s2 = "00" + to_string(minutes);
-    return s1.substr(s1.size() - 2, 2) + ":" + s2.substr(s2.size() - 2, 2);
+string printTime(int i) {
+    string hour = "00" + to_string(i / 60);
+    string minutes = "00" + to_string(i%60);
+    return hour.substr(hour.size() - 2, 2) + ":" + minutes.substr(minutes.size() - 2, 2);
 }
 
-int main()
-{
+int main() {
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> one >> s;
-        if (aScore > bScore)
-            go(aSum, s);
-        else if (bScore > aScore)
-            go(bSum, s);
-        if (one == 1)
-            aScore++;
-        else
-            bScore++;
-        prevValue = s;
+    for(int i=0; i<n; i++) {
+        cin >> o >> s;
+        if(aScore > bScore) go(aSum, s);
+        else if(aScore < bScore) go(bSum, s);
+        if(o == 1) aScore++;
+        else bScore++;
+        pre = s;
     }
-    if (aScore > bScore)
-        go(aSum, "48:00");
-    if (aScore < bScore)
-        go(bSum, "48:00");
+    if(aScore > bScore) go(aSum, "48:00");
+    else if(aScore < bScore) go(bSum, "48:00");
 
     cout << printTime(aSum) << '\n';
     cout << printTime(bSum) << '\n';
