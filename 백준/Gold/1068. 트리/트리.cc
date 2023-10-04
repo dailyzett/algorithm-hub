59bb55753d098eb6a4a5c59b9b9f6a64c8853bb9
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-10-04 21:38:17 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, root, r;
+vector<int> adj[51];
+
+int dfs(int here) {
+    int ret = 0;
+    int child = 0;
+    for(int there: adj[here]) {
+        if(there == r) continue;
+        ret += dfs(there);
+        child++;
+    }
+    if(child == 0) return 1;
+    return ret;
+}
+
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        int temp;
+        cin >> temp;
+        if (temp == -1)
+            root = i;
+        else
+            adj[temp].push_back(i);
+    }
+
+    cin >> r;
+    if(r == root) {
+        cout << 0 << '\n';
+        exit(0);
+    }
+    cout << dfs(root) << '\n';
+    return 0;
+}
