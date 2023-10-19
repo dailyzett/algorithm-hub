ce27247bc53fbbed095444d4bae471e4e6079596
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-10-19 22:16:04 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 2908 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int a[3];
-int visited[61][61][61];
-int mutalAttack[6][3] = {
-    {9, 3, 1},
-    {9, 1, 3},
+int n, a[3];
+int attack[6][3] = {
+    {1, 3, 9},
+    {1, 9, 3},
     {3, 1, 9},
     {3, 9, 1},
-    {1, 3, 9},
-    {1, 9, 3}
+    {9, 1, 3},
+    {9, 3, 1}
 };
-struct A {
-    int a,b,c;
-};
+
+struct t { int a, b, c;};
+int visited[61][61][61];
 
 int main() {
     cin >> n;
@@ -22,14 +20,9 @@ int main() {
         cin >> a[i];
     }
 
-    queue<A> q;
-    
-    int a1 = a[0];
-    int a2 = a[1];
-    int a3 = a[2];
-
-    visited[a1][a2][a3] = 1;
-    q.push({a1, a2, a3});
+    queue<t> q;
+    visited[a[0]][a[1]][a[2]] = 1;
+    q.push({a[0], a[1], a[2]});
     while(q.size()) {
         int a = q.front().a;
         int b = q.front().b;
@@ -37,13 +30,15 @@ int main() {
         q.pop();
         if(visited[0][0][0]) break;
         for(int i=0; i<6; i++) {
-            int na = max(0, a - mutalAttack[i][0]);
-            int nb = max(0, b - mutalAttack[i][1]);
-            int nc = max(0, c - mutalAttack[i][2]);
+            int na = max(0, a - attack[i][0]);
+            int nb = max(0, b - attack[i][1]);
+            int nc = max(0, c - attack[i][2]);
             if(visited[na][nb][nc]) continue;
             visited[na][nb][nc] = visited[a][b][c] + 1;
             q.push({na, nb, nc});
         }
     }
     cout << visited[0][0][0] - 1 << '\n';
+    return 0;
 }
+
