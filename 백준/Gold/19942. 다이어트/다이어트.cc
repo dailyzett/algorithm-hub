0db8c48fe71f14c178c52b6dc4ff6a4de9984857
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-11-11 10:53:16 +0900

    [Gold IV] Title: 다이어트, Time: 12 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, mp, mf, ms, mv;
+int p, f, s, v, c;
+int a[16][6];
+vector<int> retV;
+int retPrice = INT_MAX;
+map<int, vector<vector<int>>> retMap;
+
+int main() {
+    cin >> n;
+    cin >> mp >> mf >> ms >> mv;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < (1 << n); i++) {
+        int tp = 0, tf = 0, ts = 0, tv = 0;
+        int totalPrice = 0;
+        vector<int> tempV;
+
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                tempV.push_back(j + 1);
+                tp += a[j][0];
+                tf += a[j][1];
+                ts += a[j][2];
+                tv += a[j][3];
+                totalPrice += a[j][4];
+            }
+        }
+
+        if (tp >= mp && tf >= mf && ts >= ms && tv >= mv) {
+            if (retPrice >= totalPrice) {
+                retPrice = totalPrice;
+                retMap[retPrice].push_back(tempV);
+            }
+        }
+    }
+
+    if (retPrice == INT_MAX) {
+        cout << "-1";
+    } else {
+        sort(retMap[retPrice].begin(), retMap[retPrice].end());
+        cout << retPrice << '\n';
+        for (int a : retMap[retPrice][0]) cout << a << " ";
+    }
+    return 0;
+}
