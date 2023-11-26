e6d102425dd7f69e80c7991a7d3013bb4043afe2
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-11-26 16:19:46 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 332 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -2,25 +2,26 @@
 using namespace std;
 
 typedef long long ll;
+
 ll n, temp, ret;
 stack<pair<ll, ll>> st;
 int main() {
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (ll i = 0; i < n; i++) {
         cin >> temp;
         int cnt = 1;
-        while (st.size() && st.top().first <= temp) {
+        while (!st.empty() && st.top().first <= temp) {
             ret += st.top().second;
-            if (temp == st.top().first) {
+            if (st.top().first == temp) {
                 cnt = st.top().second + 1;
             } else {
                 cnt = 1;
             }
             st.pop();
         }
-        if (st.size()) ret++;
+        if (!st.empty()) ret++;
         st.push({temp, cnt});
     }
+
     cout << ret << '\n';
-    return 0;
 }
