e8b5d8c17cfcb4d80357e17da329cb5da9b323f3
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-11-26 16:12:32 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 332 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;  
-ll n, ret, temp;
-stack<pair<ll, ll>> s; 
+
+typedef long long ll;
+ll n, temp, ret;
+stack<pair<ll, ll>> st;
 int main() {
-	cin >> n;  
-	for(int i = 0; i < n; i++){
-		cin >> temp;
-		int cnt = 1; 
-		while(s.size() && s.top().first <= temp){
-			ret += s.top().second; 
-			if(s.top().first == temp){ 
-				cnt = s.top().second + 1;  
-			}else{  
-				cnt = 1; 
-			}
-			s.pop(); 
-		}
-		if(s.size()) ret++;
-		s.push({temp, cnt});
-	}
-	cout << ret << '\n';
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        int cnt = 1;
+        while (st.size() && st.top().first <= temp) {
+            ret += st.top().second;
+            if (temp == st.top().first) {
+                cnt = st.top().second + 1;
+            } else {
+                cnt = 1;
+            }
+            st.pop();
+        }
+        if (st.size()) ret++;
+        st.push({temp, cnt});
+    }
+    cout << ret << '\n';
     return 0;
-} 
+}
