b8543b6a5876ddde9e45d1a932a7f4d8b7222a1f
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-11-27 21:18:05 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+
+    cout.precision(12);  // 소수점 이하의 정확도를 높이기 위해 precision 설정
+    cout << A / B << endl;
+
+    return 0;
+}
