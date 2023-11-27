0b06b8d6086f95960c2354b8074a5bc1826ae3da
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-11-27 21:26:51 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int buddhistYear;
+    cin >> buddhistYear;
+
+    int christianYear = buddhistYear - 543;  // 불기 연도에서 543 빼기
+    cout << christianYear;
+
+    return 0;
+}
