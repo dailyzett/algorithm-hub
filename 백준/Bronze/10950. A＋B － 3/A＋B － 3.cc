5aac1223e1127fbd214908fa12fb6cf44865e396
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-11-29 19:48:22 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, t;
+int main() {
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
