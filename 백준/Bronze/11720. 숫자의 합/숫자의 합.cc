06a00aec7f1b675904dce317ef98a36daf799b47
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-12-03 10:14:09 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int sum = 0;
+    for (char c : s) {
+        int temp = c - '0';
+        sum += temp;
+    }
+    cout << sum;
+}
