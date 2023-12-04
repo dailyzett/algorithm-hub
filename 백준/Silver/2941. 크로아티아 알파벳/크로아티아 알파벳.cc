a243570d3692051bd3939553daf2274de79755de
김세준 <93368832+dailyzett@users.noreply.github.com>
2023-12-04 21:31:07 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+    string s;
+    cin >> s;
+
+    for (string& c : a) {
+        while (s.find(c) != string::npos) {
+            s.replace(s.find(c), c.length(), "@");
+        }
+    }
+
+    cout << s.length() << '\n';
+}
