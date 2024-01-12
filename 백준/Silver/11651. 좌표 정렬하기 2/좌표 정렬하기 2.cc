74d64601b792765d164ccac3266516194d0e4d5e
김세준 <93368832+dailyzett@users.noreply.github.com>
2024-01-12 20:34:27 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        v.push_back({a, b});
+    }
+
+    sort(v.begin(), v.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        if (a.second == b.second) {
+            return a.first < b.first;
+        }
+        return a.second < b.second;
+    });
+
+    for (auto p : v) {
+        cout << p.first << ' ' << p.second << '\n';
+    }
+    return 0;
+}
