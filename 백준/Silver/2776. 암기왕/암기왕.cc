ac3ef8b334b398660e95af33bc31879e9581b16f
김세준 <93368832+dailyzett@users.noreply.github.com>
2024-03-27 22:02:44 +0900

    [Silver IV] Title: 암기왕, Time: 600 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-ll t, n, m;
+int t, n, m;
+
+bool check(int temp, vector<int> &v) {
+    int low = 0, high = v.size() - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (temp == v[mid])
+            return true;
+        else if (v[mid] > temp)
+            high = mid - 1;
+        else if (v[mid] < temp)
+            low = mid + 1;
+    }
+    return false;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -10,17 +24,19 @@ int main() {
     cin >> t;
     while (t--) {
         cin >> n;
-        unordered_map<int, bool> mp;
+        vector<int> v;
         for (int i = 0; i < n; i++) {
-            int a;
-            cin >> a;
-            mp[a] = true;
+            int temp;
+            cin >> temp;
+            v.push_back(temp);
         }
+
+        sort(v.begin(), v.end());
         cin >> m;
         for (int i = 0; i < m; i++) {
-            int a;
-            cin >> a;
-            if (mp[a])
+            int temp;
+            cin >> temp;
+            if (check(temp, v))
                 cout << '1' << '\n';
             else
                 cout << '0' << '\n';
