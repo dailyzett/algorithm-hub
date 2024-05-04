3a16fd5a4ee29b31de3137bbb6bcde5de3d63baa
김세준 <93368832+dailyzett@users.noreply.github.com>
2024-05-04 21:19:22 +0900

    [Gold I] Title: 냅색문제, Time: 4 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.cc
+++ 백준/Gold/1450. 냅색문제/냅색문제.cc
@@ -5,7 +5,6 @@ int n, c, ret;
 int a[31];
 vector<int> v1, v2;
 
-// 가방 무게를 합산한 v1, v2
 void go(int here, int mx, vector<int> &v, int sum) {
     if (sum > c) return;
     if (here > mx) {
@@ -15,6 +14,7 @@ void go(int here, int mx, vector<int> &v, int sum) {
 
     go(here + 1, mx, v, sum + a[here]);
     go(here + 1, mx, v, sum);
+
     return;
 }
 
@@ -26,6 +26,7 @@ int main() {
 
     go(0, n / 2 - 1, v1, 0);
     go(n / 2, n - 1, v2, 0);
+
     sort(v1.begin(), v1.end());
     sort(v2.begin(), v2.end());
 
@@ -33,5 +34,4 @@ int main() {
         if (c - v >= 0) ret += ((int)(upper_bound(v2.begin(), v2.end(), c - v) - v2.begin()));
     }
     cout << ret << '\n';
-    return 0;
 }
