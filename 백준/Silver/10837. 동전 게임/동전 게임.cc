b92f44316405e7e8a975da108c4f20b09a641857
김세준 <93368832+dailyzett@users.noreply.github.com>
2024-06-03 21:42:09 +0900

    [Silver III] Title: 동전 게임, Time: 20 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10837. 동전 게임/동전 게임.cc
+++ 백준/Silver/10837. 동전 게임/동전 게임.cc
@@ -3,6 +3,8 @@ using namespace std;
 
 int k, c, n, m;
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cin >> k;
     cin >> c;
     for (int i = 0; i < c; i++) {
@@ -19,19 +21,17 @@ int main() {
             if (diff - remain_game <= 2) {
                 cout << "1" << '\n';
                 continue;
-            } else {
-                cout << "0" << '\n';
-                continue;
             }
+            cout << "0" << '\n';
+            continue;
         }
 
         if (diff - remain_game <= 1) {
             cout << "1" << '\n';
             continue;
-        } else {
-            cout << "0" << '\n';
-            continue;
         }
+
+        cout << 0 << '\n';
     }
 
     return 0;
